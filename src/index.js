d2ec09002a57dc7ed8a65294f5f60f1dd774f2d3
Ziwa Mukungu <zmukungu@gmail.com>
2018-05-15 09:52:11 -0400

    change background height to 100vh

--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ import {BrowserRouter} from 'react-router-dom'
 import App from './App'
 import './styles/index.css'
 
-ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>, document.getElementById('root'))
+ReactDOM.render(
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<App />
+	</BrowserRouter>, document.getElementById('root'))
